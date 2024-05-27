fd29f4134173eb9564d082601c4a9a897e0558ec
zjy <249548243@qq.com>
2024-05-27 23:54:00 +0800

    Initial commit

---
+++ stadium-management-backend/src/test/java/com/example/stadiummanagementbackend/StadiumManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stadiummanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadiumManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
